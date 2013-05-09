eb36c10142590cae7db51307eed8a8085a9fc4f3
Robeen Dey <robeen@djcoursesonline.com>
2013-05-08 20:33:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bgiv::Application.config.secret_token = '778824546ecc95c367b0862a28a895227b25888be723137bc0a5ad9c73352b8541b8d1b1f5f42242e3deb298621dcee70cb2062027e49565e0d9989496b01a6c'
